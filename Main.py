57b1db6a0d174e8b95187b0a00ed18f74640a61f
Clement Gouriou <clementgouriou@orange.com>
2019-09-02 13:49:32 +0200

    Update on the Main.py

--- Main.py
+++ Main.py
@@ -25,9 +25,11 @@ class PhotonicNetwork():
         self.input     = {}
         self.output    = {}
         self.networMat = None
+        self.networLayers = {}
     # --- define external method --- #
     allOutputStatefromNetwork = OutputStateFromInputStateAndNetworkMatrix
-    makeBeamSplitter          = beamSplitter2Modes
+    matrixPhaseShift          = matrixPhaseShift
+    matrixSwitchingOutput     = matrixSwitchingOutput
     # ---  --- #
 
     def setInputSate(self, inputState):
@@ -42,13 +44,61 @@ class PhotonicNetwork():
     def addElementBetweenChannels(self, ch_list, element):
         return None
 
-    def makeBeamSplitter(self):
+    def BS(self):
         '''
         return a 2x2 np.array representing a 50:50 balanced beam splitter
         '''
-        BS = 1/np.sqrt(2) * np.array([[1, 1j],[1j, 1]])
-        #BS = 1/np.sqrt(2) * np.array([[1, 1],[-1, 1]])
-        return BS
+        return 1/np.sqrt(2) * np.array([[1, 1j],[1j, 1]])
+
+    def PS(self,  mode_phase_dic):
+        '''
+        return the square matrix for adding phase shifts specified by mode_phase_list:
+            -  mode_phase_dic = { mode_nbr : phase_shift , ...}
+        '''
+        if self.channel_nbr == 0:
+            print('ERROR in PS: the network has no channels.')
+            return None
+        mode_phase_list = []
+        for key in mode_phase_dic:
+            mode_phase_list.append([key, mode_phase_dic[key]])
+        PS_mat = matrixPhaseShift(self.channel_nbr, mode_phase_list)
+        return PS_mat
+
+    def switchMat(self, mode_switch_dic):
+        '''
+        return the square matrix that commute two outputs.
+        It re-organises the given input into the desired output,
+            - mode_switch_dic = {input : output , ... }
+        '''
+        if self.channel_nbr == 0:
+            print('ERROR in switchMat: the network has no channels.')
+            return None
+        mode_switch_list = []
+        for key in mode_switch_dic:
+            mode_switch_list.append([key, mode_switch_dic[key]])
+        return matrixSwitchingOutput(self.channel_nbr, mode_switch_list)
+
+    def initNetworkMatrix(self, T=None):
+        '''
+        Initiat the network matrix with unity matrix by default.
+        Also initiat the channel_nbr.
+        '''
+        if   type(T) == type(None):
+            if self.channel_nbr != 0:
+                self.networMat   = np.eye(self.channel_nbr)
+            else:
+                print('ERROR in initNetworkMatrix: no channel to initiat the netwrk.')
+                return None
+        else:
+            self.networMat   = T.copy()
+            self.channel_nbr = T.shape[0]
+
+    def addNetworkLayer(self, T):
+        s = T.shape
+        if s[0] != s[1] or s[0] != self.channel_nbr:
+            print('ERROR in addNetworkLayer: new layer matrix is not the same size than the network.')
+            return None
+        self.networMat = np.dot(self.networMat, T)
 
     def buildNetworkMatrix(self):
         N = len(list(self.channels.keys()))
@@ -88,6 +138,10 @@ class PhotonicNetwork():
         return post_selected_state
 
     def simulatePhotonicNetwork(self, input_state=None, network_mat=None, print_all=False):
+        '''
+        Compute the final state resulting from the input state in the photonic network.
+        It returns a sum of all the non-zero states in the second quantisation formalism, ie in terms of modes occupation.
+        '''
         if   type(input_state) == type(None) and len(list(self.input.keys())) != 0:
             input_state = self.input
         elif type(input_state) != type(dict([])):
@@ -98,7 +152,12 @@ class PhotonicNetwork():
         elif type(network_mat) != type(np.array([])):
             print('Error in simulatePhotonicNetwork: input_state not valid.')
             return None
-        # ---  --- #
+        # --- verification --- #
+        key = list(input_state.keys())[0]
+        if network_mat.shape[0] != network_mat.shape[1] or len(input_state[key][1]) != network_mat.shape[0]:
+            print('ERRROR in simulatePhotonicNetwork: wrong dimension for input or matrix network.')
+            return None
+        # --- simulation --- #
         self.output = self.allOutputStatefromNetwork( input_state, network_mat , printable=print_all)
         self.visualisationSecondQuantisation( self.output )
 
@@ -109,7 +168,8 @@ if __name__=='__main__':
     print('STARTING')
     PN = PhotonicNetwork()
     # ---  --- #
-    BS = PN.makeBeamSplitter()
-    PN.setNetworkMatrix(BS)
-    PN.simulatePhotonicNetwork( {1 : [1., [3,1] ]} , BS)
+    PN.initNetworkMatrix(np.eye(4))
+    PN.addNetworkLayer( makeMatDiagFromSquareMatrices([1.,PN.BS(),1.]) )
+    PN.addNetworkLayer( makeMatDiagFromSquareMatrices([PN.BS(),PN.BS()]) )
+    PN.simulatePhotonicNetwork( {1 : [1., [0,1,1,0] ]})
 
